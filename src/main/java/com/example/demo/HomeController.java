b1f2766db3a226d0af43d7491c6af424974ce3b1
hamnaasif5 <hamna.asif245@gmail.com>
2019-08-23 12:00:59 -0400

    df

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -45,6 +46,22 @@ public class HomeController {
           }
           messageRepository.save(message);
           return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showMessage (@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message" , messageRepository.findById(id).get());
+        return "messageform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteMessage(@PathVariable("id") long id, Model model){
+        messageRepository.deleteById(id);
+        return "redirect:/";
     }
 
 
